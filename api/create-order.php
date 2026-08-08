b05f878c3b0bed6987ebb12f2b2da2db0d0b2526
TunarKerimli <tunarkerimli1501@gmail.com>
2026-08-08 19:08:22 +0400

    Add files via upload

--- api/create-order.php
+++ api/create-order.php
@@ -1,6 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://purpleuniverse.site");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(204);
+    exit;
+}
 
 require_once "../config/database.php";
 
